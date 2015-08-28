0065118b043f347df5903a9d479107cf9295a46a
Jonghoon,Kim <kim0070@gmail.com>
2015-08-28 20:53:51 +0900

    spring security access denied handling add

--- src/main/java/net/java_school/controller/HomeController.java
+++ src/main/java/net/java_school/controller/HomeController.java
@@ -7,8 +7,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HomeController {
-    @RequestMapping(method=RequestMethod.GET)
-    public String index() {
-        return "index";
-    }
+
+	@RequestMapping(method=RequestMethod.GET)
+	public String index() {
+		return "index";
+	}
+
+	@RequestMapping(value="/403", method=RequestMethod.GET)
+	public String error403() {
+		return "403";
+	}
+
 }
